95bed5ecbdd915fe45e11c008f281e7c0a9ee2d7
as04-CyberNova <abhyudayasinha4@gmail.com>
2026-01-20 00:28:42 +0530

    simple_interest_for.c

---
+++ Loops /simple_interest_for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int p,n,count;
+    float r,si;
+    for(count = 1; count<=3; count = count+1){
+        printf("Enter values of p,n and r: ");
+        scanf("%d %d %f",&p,&n,&r);
+        si = (p*n*r)/100;
+        printf("Simple interest = Rs.%.2f\n",si);
+    }
+    return 0;
+}
