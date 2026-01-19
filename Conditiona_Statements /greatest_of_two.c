7f4f670169875f1eae4f8041785e66a9ad4fb365
as04-CyberNova <abhyudayasinha4@gmail.com>
2026-01-20 00:21:21 +0530

    greatest_of_two.c

---
+++ Conditiona_Statements /greatest_of_two.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int x, y;
+    printf("Enter x and y: ");
+    scanf("%d%d",&x, &y);
+    if(x>y)
+    printf("The greater number is: %d\n",x);
+    else if(x<y)
+    printf("The greater number is: %d\n",y);
+    else
+    printf("Both of them are equal: %d\n",x);
+    return 0;
+}
